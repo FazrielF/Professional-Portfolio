97f7c26e942a71cd1d3551fb81b8f7fe2f0a1f9f
Fazriel <iharukax@gmail.com>
2026-06-17 07:28:32 +0700

    small changes on projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -13,25 +13,25 @@ export const projects = [
     id: 2,
     title: "Fake Store App",
     description:
-      "Mini project an E-Commerce website.",
+      "A fake App Store website built to replicate a mobile application marketplace, featuring app browsing, filtering, and a clean user interface.",
     tech: ["React", "Tailwind", "Flowbite", "Etc"],
-    link: "https://github.com/FazrielF/Fake-Store",
+    link: "https://github.com/FazrielF/FE-Pengaduan",
     image: "/project/Fake-Store.png",
   },
   {
     id: 3,
-    title: "Ghibli Studio",
+    title: "Pengaduan Masyarakat",
     description:
-      "React project using Ghibli Studio API",
-    tech: ["React", "Tailwind", "Flowbite", "Etc"],
+      "A Public Complaints Website built with React and Express.js, designed to make it easier for citizens to report local issues..",
+    tech: ["React", "Tailwind", "Javascript", "Etc"],
     link: "https://github.com/FazrielF",
-    image: "/project/Ghibli-Studio.png",
+    image: "/project/Pengaduan-Masyarakat.png",
   },
   {
     id: 4,
     title: "Reactive Liquid",
     description:
-      "My first mini project using Touch Designer.",
+      "An interactive TouchDesigner installation that transforms user body movements into fluid dynamics. By capturing motion data, the project generates a liquid-like silhouette that dynamically follows and reacts to the user's gestures in real time.",
     tech: ["Touch Designer", "Etc"],
     link: "https://github.com/FazrielF",
     image: "/project/Reactive-Liquid.png",
@@ -40,7 +40,7 @@ export const projects = [
     id: 5,
     title: "Magnetic Particles",
     description:
-      "My second mini project using Touch Designer.",
+      "A gesture-controlled interactive system built in TouchDesigner and integrated with MediaPipe. It allows users to manipulate and attract computer-generated particles in real time using a hand-pinch gesture, creating a 'magnetic' pull effect based on hand tracking.",
     tech: ["Touch Designer", "Etc"],
     link: "https://github.com/fazriel",
     image: "/project/Magnetic-Particle.png",
