1c8b86d5e7992b6880292432a92fc64da62d6922
Fazriel <iharukax@gmail.com>
2026-06-30 18:23:39 +0700

    change photo

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -25,7 +25,7 @@ export default function About() {
 
             {/* Image */}
             <img
-              src="/Fazriel.jpg"
+              src="/Fazriel.jpeg"
               alt="Profile"
               className="w-full h-[420px] object-cover border-2 border-dark"
             />
